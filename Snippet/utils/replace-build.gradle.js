f62e594cbefea5141811b522887d2b73d6777b7f
TaumuLu <972409545@qq.com>
2020-03-24 19:57:43 +0800

    Create replace-build.gradle.js

---
+++ Snippet/utils/replace-build.gradle.js
@@ -0,0 +1,29 @@
+const { existsSync, readFileSync, writeFileSync } = require('fs')
+const { join } = require('path')
+
+const moduleList = ['@gregfrench/react-native-wheel-picker']
+
+const rootDir = join(__dirname, '..')
+const nodeModulesPath = join(rootDir, 'node_modules')
+const propertyName = ['compileSdkVersion', 'buildToolsVersion', 'minSdkVersion', 'targetSdkVersion']
+const reg = new RegExp(`(${propertyName.join('|')}) (.*)`, 'g')
+const defText = `def safeExtGet(prop, fallback) {
+  rootProject.ext.has(prop) ? rootProject.ext.get(prop) : fallback
+}
+`
+
+function replace(list) {
+  list.forEach(name => {
+    const filePath = join(nodeModulesPath, name, 'android', 'build.gradle')
+    if (existsSync(filePath)) {
+      const data = readFileSync(filePath, 'utf-8')
+      let value = data.replace(reg, (_o, c1, c2) => `${c1} safeExtGet("${c1}", ${c2})`)
+      if (!value.startsWith(defText)) {
+        value = `${defText}\n${value}`
+      }
+      writeFileSync(filePath, value)
+    }
+  })
+}
+
+replace(moduleList)
